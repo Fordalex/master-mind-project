2fcf955e34150e49be4aabfc1f78cd67f243a3a8
Alex Ford <alex96ford19@gmail.com>
2019-10-02 21:40:06 +0100

    checkCounters function complete

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,15 +7,15 @@ const yellowCounter = '<div class="yellow-counter"></div>';
 const blueCounter = '<div class="blue-counter"></div>';
 const holeCounter = '<div class="hole-counter"></div>';
 
-var playerLocationOne = 0; 
-var playerLocationTwo = 0; 
-var playerLocationThree = 0; 
-var playerLocationFour = 0; 
+var playerLocationOne = 0;
+var playerLocationTwo = 0;
+var playerLocationThree = 5;
+var playerLocationFour = 2;
 
-var masterLocationOne = 0; 
-var masterLocationTwo = 0; 
-var masterLocationThree = 0; 
-var masterLocationFour = 0; 
+var masterLocationOne = 0;
+var masterLocationTwo = 1;
+var masterLocationThree = 0;
+var masterLocationFour = 0;
 
 generateCounters();
 
@@ -59,6 +59,30 @@ function removeCounter() {
 };
 
 function checkCounters() {
-    if (playerLocationOne == masterLocationOne)
+    if (playerLocationOne == masterLocationOne) {
+        console.log('one-black');
+    } else if (playerLocationOne == masterLocationTwo || playerLocationOne == masterLocationThree || playerLocationOne == masterLocationFour) {
+        console.log('one-white');
+    }
+    if (playerLocationTwo == masterLocationTwo) {
+        console.log('two-black');
+    } else if (playerLocationTwo == masterLocationOne || playerLocationTwo == masterLocationThree || playerLocationTwo == masterLocationFour) {
+        console.log('two-white');
+    }
+    if (playerLocationThree == masterLocationThree) {
+        console.log('three-black');
+    } else if (playerLocationThree == masterLocationOne || playerLocationThree == masterLocationTwo || playerLocationThree == masterLocationFour) {
+        console.log('three-white');
+    }
+    if (playerLocationFour == masterLocationFour) {
+        console.log('four-black');
+    } else if (playerLocationFour == masterLocationTwo || playerLocationFour == masterLocationTwo || playerLocationFour == masterLocationThree) {
+        console.log('four-white');
+    }
+
+
+
+
+
 };
 
