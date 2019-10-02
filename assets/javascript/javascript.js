a2915a679c8b84485c7ee5aab9ef86e6a917c950
Alex Ford <alex96ford19@gmail.com>
2019-10-02 22:38:44 +0100

    getting the javascript to check order and colour of the counter fixed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,37 +9,45 @@ const holeCounter = '<div class="hole-counter"></div>';
 
 var playerLocationOne = 0;
 var playerLocationTwo = 0;
-var playerLocationThree = 5;
-var playerLocationFour = 2;
+var playerLocationThree = 0;
+var playerLocationFour = 0;
 
 var masterLocationOne = 0;
-var masterLocationTwo = 1;
+var masterLocationTwo = 0;
 var masterLocationThree = 0;
 var masterLocationFour = 0;
+
+
 
 generateCounters();
 
 function generateCounters() {
-    $('#master-one').html('');
-    $('#master-two').html('');
-    $('#master-three').html('');
-    $('#master-four').html('');
+    masterLocationOne = Math.floor(Math.random() * 5);
+    masterLocationTwo = Math.floor(Math.random() * 5);
+    masterLocationThree = Math.floor(Math.random() * 5);
+    masterLocationFour = Math.floor(Math.random() * 5);
+    console.log(masterLocationOne,masterLocationTwo,masterLocationThree,masterLocationFour);
 };
 
-function chooseColour(chosenColour) {
+function chooseColour(chosenColour,colourNum) {
     if (round == 1 && holeSelect == 1) {
         $('#one-one').html(chosenColour);
         holeSelect++;
+        playerLocationOne = colourNum;
     } else if (round == 1 && holeSelect == 2) {
         $('#one-two').html(chosenColour);
         holeSelect++;
+        playerLocationTwo = colourNum;
     } else if (round == 1 && holeSelect == 3) {
         $('#one-three').html(chosenColour);
         holeSelect++;
+        playerLocationThree = colourNum;
     } else if (round == 1 && holeSelect == 4) {
         $('#one-four').html(chosenColour);
         holeSelect++;
+        playerLocationFour = colourNum;
     }
+    console.log(playerLocationOne,playerLocationTwo,playerLocationThree,playerLocationFour)
 };
 
 function removeCounter() {
@@ -59,30 +67,25 @@ function removeCounter() {
 };
 
 function checkCounters() {
-    if (playerLocationOne == masterLocationOne) {
+    if (masterLocationOne == playerLocationOne) {
         console.log('one-black');
-    } else if (playerLocationOne == masterLocationTwo || playerLocationOne == masterLocationThree || playerLocationOne == masterLocationFour) {
+    } else if (masterLocationOne == playerLocationTwo || masterLocationOne == playerLocationThree || masterLocationOne == playerLocationFour) {
         console.log('one-white');
     }
-    if (playerLocationTwo == masterLocationTwo) {
+    if (masterLocationTwo == playerLocationTwo) {
         console.log('two-black');
-    } else if (playerLocationTwo == masterLocationOne || playerLocationTwo == masterLocationThree || playerLocationTwo == masterLocationFour) {
+    } else if (masterLocationTwo == playerLocationOne || masterLocationTwo == playerLocationTwo || masterLocationTwo == playerLocationThree) {
         console.log('two-white');
     }
-    if (playerLocationThree == masterLocationThree) {
+    if (masterLocationThree == playerLocationThree) {
         console.log('three-black');
-    } else if (playerLocationThree == masterLocationOne || playerLocationThree == masterLocationTwo || playerLocationThree == masterLocationFour) {
+    } else if (masterLocationThree == playerLocationOne || masterLocationThree == playerLocationTwo || masterLocationThree == playerLocationFour) {
         console.log('three-white');
     }
-    if (playerLocationFour == masterLocationFour) {
+    if (masterLocationFour == playerLocationFour) {
         console.log('four-black');
-    } else if (playerLocationFour == masterLocationTwo || playerLocationFour == masterLocationTwo || playerLocationFour == masterLocationThree) {
+    } else if (masterLocationFour == playerLocationOne || masterLocationFour == playerLocationTwo || masterLocationFour == playerLocationThree) {
         console.log('four-white');
     }
-
-
-
-
-
 };
 
--- readme.md
+++ readme.md
@@ -5,3 +5,21 @@ This is an old family game I used to play. Mastermind is a game that requires a 
 ## Link
 
 Can you beat the computer? Find out: [Mastermind](https://fordalex.github.io/master-mind-project/)
+
+## UX
+Use this section to provide insight into your UX process, focusing on who this website is for, what it is that they want to achieve and how your project is the best way to help them achieve these things.
+
+In particular, as part of this section we recommend that you provide a list of User Stories, with the following general structure:
+
+As a user type, I want to perform an action, so that I can achieve a goal.
+This section is also where you would share links to any wireframes, mockups, diagrams etc. that you created as part of the design process. These files should themselves either be included in the project itself (in an separate directory), or just hosted elsewhere online and can be in any format that is viewable inside the browser.
+
+## Features
+
+Choosing Colours - The user is able to select the colours of the pegs and put them in order on the virtual board to allow the user to guess the computers coloured pegs.
+
+### Features Left to Implement
+
+Computers coloured pegs to be generated.
+
+
